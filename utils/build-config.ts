c5ff9bfb32799d8821fa4bb53776ebaea5211030
Imackie <ianm@completesystemsupport.com>
2018-01-09 19:25:53 -0500

    commenting configs

--- utils/build-config.ts
+++ utils/build-config.ts
@@ -1,10 +1,16 @@
 
+/**
+ * Application paths used for gulp plugins.
+ */
 export const Paths = {
   electron_src: './src/electron/**/*.ts',
   electron_dest: './dist/electron/',
   app_src: ['./src/app/**/*.ts', './src/app/**/*.html', './src/app/**/*.css']
 };
 
+/**
+ * Live-Reload configuration for electron window proxy.
+ */
 export const BrowserSyncConfig = {
   open: false,
   ui: false,
